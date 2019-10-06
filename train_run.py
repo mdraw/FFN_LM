047c928bb53a21c7ef67001626f2bf225d640b45
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2019-10-06 14:43:21 -0400

    Update train_run.py

--- train_run.py
+++ train_run.py
@@ -1,31 +1,19 @@
 import argparse
 import time
 import random
-import zipfile
 from torch.utils.data import DataLoader
 from core.data.utils import *
-
+from functools import partial
+import os
 import torch
-from torch import nn
 from torch import optim
-import numpy as np
 from torch.autograd import Variable
 import torch.nn.functional as F
+import numpy as np
+from core.models.ffn import FFN
+from core.data import BatchCreator
 
 parser = argparse.ArgumentParser(description='Train a network.')
-parser.add_argument(
-    '-s', '--epoch-size', type=int, default=100,
-    help='How many training samples to process between '
-         'validation/preview/extended-stat calculation phases.'
-)
-parser.add_argument(
-    '-m', '--max-steps', type=int, default=500000,
-    help='Maximum number of training steps to perform.'
-)
-parser.add_argument(
-    '-t', '--max-runtime', type=int, default=3600 * 24 * 4,  # 4 days
-    help='Maximum training time (in seconds).'
-)
 parser.add_argument(
     '-r', '--resume', metavar='PATH',
     help='Path to pretrained model state dict or a compiled and saved '
@@ -35,9 +23,14 @@ parser.add_argument(
     '--deterministic', action='store_true',
     help='Run in fully deterministic mode (at the cost of execution speed).'
 )
-parser.add_argument('-i', '--ipython', action='store_true',
-    help='Drop into IPython shell on errors or keyboard interrupts.'
-)
+
+parser.add_argument('-d', '--data', type=str, default='./data1.h5', help='training data')
+parser.add_argument('-b', '--batch_size', type=int, default=4, help='training batch size')
+parser.add_argument('--delta', default=(5, 5, 5), help='delta offset')
+parser.add_argument('--input_size', default=(30, 30, 30), help='input size')
+parser.add_argument('--clip_grad_thr', type=float, default=0.7, help='grad clip threshold')
+parser.add_argument('--save_path', type=str, default='./model', help='model save path')
+
 args = parser.parse_args()
 
 deterministic = args.deterministic
@@ -46,109 +39,64 @@ if deterministic:
 else:
     torch.backends.cudnn.benchmark = True  # Improves overall performance in *most* cases
 
+if not os.path.exists(args.save_path):
+    os.makedirs(args.save_path)
+
 # Don't move this stuff, it needs to be run this early to work
-import core
-core.select_mpl_backend('Agg')
-logger = logging.getLogger('elektronn3log')
-
-from core.training import metrics
-from core.models.ffn import FFN
-from core.data import BatchCreator
-
-device = torch.device('cuda')
-logger.info(f'Running on device: {device}')
 
 
 def run():
-    model = FFN(in_channels=2, out_channels=1).to(device)
+    """创建模型"""
+    model = FFN(in_channels=2, out_channels=1).cuda()
 
-    save_root = os.path.expanduser('./log/ffn/')
-    os.makedirs(save_root, exist_ok=True)
+    """数据路径"""
+    input_h5data = [args.data]
 
-    input_h5data = ['./data.h5']
-
-    if args.resume is not None:  # Load pretrained network
-        pretrained = os.path.expanduser(args.resume)
-        _warning_str = 'Loading model without optimizer state. Prefer state dicts'
-        if zipfile.is_zipfile(pretrained):  # Zip file indicates saved ScriptModule
-            logger.warning(_warning_str)
-            model = torch.jit.load(pretrained, map_location=device)
-        else:  # Either state dict or pickled model
-            state = torch.load(pretrained)
-            if isinstance(state, dict):
-                model.load_state_dict(state['model_state_dict'])
-                optimizer_state_dict = state.get('optimizer_state_dict')
-                lr_sched_state_dict = state.get('lr_sched_state_dict')
-                if optimizer_state_dict is None:
-                    logger.warning('optimizer_state_dict not found.')
-                if lr_sched_state_dict is None:
-                    logger.warning('lr_sched_state_dict not found.')
-            elif isinstance(state, nn.Module):
-                logger.warning(_warning_str)
-                model = state
-            else:
-                raise ValueError(f'Can\'t load {pretrained}.')
-
-    train_dataset = BatchCreator(input_h5data, (33, 33, 33), delta=(8, 8, 8), train=True)
+    """创建data loader"""
+    train_dataset = BatchCreator(input_h5data, args.input_size, delta=args.delta, train=True)
     train_loader = DataLoader(train_dataset, shuffle=True, num_workers=1, pin_memory=True)
 
-    optimizer = optim.Adam(
+    optimizer = optim.SGD(
         model.parameters(),
         lr=1e-3,  # Learning rate is set by the lr_sched below
-        # momentum=0.9,
+        momentum=0.9,
         weight_decay=0.5e-4,
     )
 
-    valid_metrics = {
-        'val_accuracy': metrics.bin_accuracy,
-        'val_precision': metrics.bin_precision,
-        'val_recall': metrics.bin_recall,
-        'val_DSC': metrics.bin_dice_coefficient,
-        'val_IoU': metrics.bin_iou,
-    }
+    best_loss = np.inf
 
-    for iter, (data, label, seed, coor)in enumerate(train_loader):
+    """获取数据流"""
+    for itr, (seeds, images, labels, offsets) in enumerate(
+            get_batch(train_loader, args.batch_size, args.input_size, partial(fixed_offsets, fov_moves=train_dataset.shifts))):
 
-        # while(1):
-            offsets = []
-            for idx, offset in enumerate(fixed_offsets(seed, train_dataset.shifts, 0.9)):
-                offsets.append(np.array(offset))
-                if idx == 3:##bach_size-1
-                    break
-            if len(offsets) == 0:
-                break
+        input_data = torch.cat([images, seeds], dim=1)
 
-            images = np.zeros([len(offsets)]+[1, 33, 33, 33])
-            labels = np.zeros([len(offsets)]+[1, 33, 33, 33])
-            seeds = np.zeros([len(offsets)]+[1, 33, 33, 33])
-            for idx, offset in enumerate(offsets):
-                start = offset + model.radii - model.input_size // 2
-                end = start + model.input_size
-                assert np.all(start >= 0)
+        input_data = Variable(input_data.cuda())
+        seeds = seeds.cuda()
+        labels = labels.cuda()
 
-                selector = [slice(s, e) for s, e in zip(start, end)]
-                images[idx][0] = data[0][selector]
-                labels[idx][0] = label[0][selector]
-                seeds[idx][0] = seed[0][selector]
+        logits = model(input_data)
 
-            images = torch.from_numpy(images).float()
-            labels = torch.from_numpy(labels).float()
-            seeds = torch.from_numpy(seeds).float()
-            input_data = torch.cat([images, seeds], dim=1)
+        updated = seeds + logits
+        optimizer.zero_grad()
+        loss = F.binary_cross_entropy_with_logits(updated, labels)
+        loss.backward()
+        """梯度截断"""
+        torch.nn.utils.clip_grad_norm(model.parameters(), args.clip_grad_thr)
 
-            input_data = Variable(input_data.cuda())
-            seeds = seeds.cuda()
-            labels = labels.cuda()
+        optimizer.step()
 
-            logits = model(input_data)
+        diff = (updated.sigmoid()-labels).detach().cpu().numpy()
+        accuracy = 1.0*(diff < 0.001).sum() / np.prod(labels.shape)
+        print("loss: {}, offset: {}, Accuracy: {:.2f}% ".format(loss.item(), itr, accuracy.item()*100))
 
-            updated = seeds + logits
-            optimizer.zero_grad()
-            loss = F.binary_cross_entropy_with_logits(updated, labels)
-            loss.backward()
-            optimizer.step()
-            print("loss: {}, offset: {}".format(loss.item(), offsets))
-            update_seed(updated, seed, model, offsets)
+        # update_seed(updated, seeds, model, offsets)
+        # seed = updated
+        """根据最佳loss并且保存模型"""
+        if best_loss > loss.item():
+            best_loss = loss.item()
+            torch.save(model.state_dict(), os.path.join(args.save_path, 'ffn.pth'))
+            print('Model saved!')
 
 
 if __name__ == "__main__":
