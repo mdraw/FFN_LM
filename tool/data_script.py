268bfa6158dd30a25010eface84ac6b4a625a030
x903102883 <x903102883@gmail.com>
2019-10-08 17:50:16 +0000

    good2

---
+++ tool/data_script.py
@@ -0,0 +1,148 @@
+import argparse
+from collections import defaultdict
+import h5py
+import numpy as np
+import tifffile
+import cv2
+
+parser = argparse.ArgumentParser('script to generate training data')
+parser.add_argument('--image', type=str, default='./data/ffn/images/raw_data_4_channel.tif', help='image data path')
+parser.add_argument('--label', type=str, default='./data/ffn/labels/target_data.tif', help='label data path')
+parser.add_argument('--save', type=str, default='data1.h5', help='save file name')
+parser.add_argument('--shape', type=list, default=[41, 41, 41], help='seed shape')
+parser.add_argument('--thr', type=list, default=[0.025, 0.05, 0.075, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+parser.add_argument('--min_size', type=int, default=1000)
+
+args = parser.parse_args()
+
+
+def _query_summed_volume(svt, diam):
+    """Queries a summed volume table.
+
+    Operates in 'VALID' mode, i.e. only computes the sums for voxels where the
+    full diam // 2 context is available.
+
+    Args:
+    svt: summed volume table (see _summed_volume_table)
+    diam: diameter (z, y, x tuple) of the area within which to compute sums
+
+    Returns:
+    sum of all values within a diam // 2 radius (under L1 metric) of every voxel
+    in the array from which 'svt' was built.
+    """
+    return (
+        svt[diam[0]:, diam[1]:, diam[2]:] - svt[diam[0]:, diam[1]:, :-diam[2]] -
+        svt[diam[0]:, :-diam[1], diam[2]:] - svt[:-diam[0], diam[1]:, diam[2]:] +
+        svt[:-diam[0], :-diam[1], diam[2]:] + svt[:-diam[0], diam[1]:, :-diam[2]]
+        + svt[diam[0]:, :-diam[1], :-diam[2]] -
+        svt[:-diam[0], :-diam[1], :-diam[2]])
+
+
+def _summed_volume_table(val):
+    """Computes a summed volume table of 'val'."""
+    val = val.astype(np.int32)
+    svt = val.cumsum(axis=0).cumsum(axis=1).cumsum(axis=2)
+    return np.pad(svt, [[1, 0], [1, 0], [1, 0]], mode='constant')
+
+
+def clear_dust(data, min_size=10):
+    """Removes small objects from a segmentation array.
+
+    Replaces objects smaller than `min_size` with 0 (background).
+
+    Args:
+    data: numpy array of segment IDs
+    min_size: minimum size in voxels of an object to be retained
+
+    Returns:
+    the data array (modified in place)
+    """
+    ids, sizes = np.unique(data, return_counts=True)
+    small = ids[sizes < min_size]
+    small_mask = np.in1d(data.flat, small).reshape(data.shape)
+    data[small_mask] = 0
+    return data
+
+
+def compute_partitions(seg_array, thresholds, lom_radius, min_size=10000):
+    seg_array = clear_dust(seg_array, min_size=min_size)
+    assert seg_array.ndim == 3
+
+    lom_radius = np.array(lom_radius)
+    lom_radius_zyx = lom_radius[::-1]
+    lom_diam_zyx = 2 * lom_radius_zyx + 1
+
+    def _sel(i):
+        if i == 0:
+            return slice(None)
+        else:
+            return slice(i, -i)
+
+    valid_sel = [_sel(x) for x in lom_radius_zyx]
+    output = np.zeros(seg_array[valid_sel].shape, dtype=np.uint8)
+    corner = lom_radius
+
+    labels = set(np.unique(seg_array))
+
+    fov_volume = np.prod(lom_diam_zyx)
+    for l in labels:
+        # Don't create a mask for the background component.
+        if l == 0:
+            continue
+
+        object_mask = (seg_array == l)
+
+        svt = _summed_volume_table(object_mask)
+        active_fraction = _query_summed_volume(svt, lom_diam_zyx) / fov_volume
+        assert active_fraction.shape == output.shape
+
+        # Drop context that is only necessary for computing the active fraction
+        # (i.e. one LOM radius in every direction).s
+        object_mask = object_mask[valid_sel]
+
+        # TODO(mjanusz): Use np.digitize here.
+        for i, th in enumerate(thresholds):
+            output[object_mask & (active_fraction < th) & (output == 0)] = i + 1
+
+        output[object_mask & (active_fraction >= thresholds[-1]) & (output == 0)] = len(thresholds) + 1
+
+    return corner, output
+
+
+def run():
+    images = tifffile.TiffFile(args.image).asarray()
+    labels = tifffile.TiffFile(args.label).asarray()
+    images = np.array([cv2.cvtColor(im, cv2.COLOR_BGR2GRAY) for im in images])
+    labels = np.array([cv2.cvtColor(label, cv2.COLOR_BGR2GRAY) for label in labels])
+
+    m = np.array([int(x/2) for x in args.shape])
+    seg = labels.copy()
+    corner, partitions = compute_partitions(seg[...], [float(x) for x in args.thr], m, args.min_size)
+
+    totals = defaultdict(int)  # partition -> voxel count
+    indices = defaultdict(list)  # partition -> [(vol_id, 1d index)]
+    vol_shapes = partitions.shape
+    uniques, counts = np.unique(partitions, return_counts=True)
+    for val, cnt in zip(uniques, counts):
+        if val == 255:
+            continue
+
+        totals[val] += cnt
+        indices[val].extend([flat_index for flat_index in np.flatnonzero(partitions == val)])
+
+    max_count = max(totals.values())
+    indices = np.concatenate([np.resize(np.random.permutation(v), max_count) for v in indices.values()], axis=0)
+    np.random.shuffle(indices)
+    coor = []
+    for coord_idx in indices:
+        z, y, x = np.unravel_index(coord_idx, vol_shapes)
+        coor.append([z+m[2], y+m[1], x+m[0]])
+
+    with h5py.File(args.save, 'w') as f:
+        f.create_dataset('image', data=images, compression='gzip')
+        f.create_dataset('label', data=labels, compression='gzip')
+        f.create_dataset('coor', data=coor, compression='gzip')
+
+
+if __name__ == '__main__':
+    run()
---
+++ train_neurodata.py
@@ -0,0 +1,136 @@
+import argparse
+import time
+import random
+from torch.utils.data import DataLoader
+from core.data.utils import *
+from functools import partial
+import os
+import torch
+from torch import optim
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+from core.models.ffn import FFN
+from core.data import BatchCreator
+import sys
+
+import h5py
+parser = argparse.ArgumentParser(description='Train a network.')
+parser.add_argument('--deterministic', action='store_true',
+    help='Run in fully deterministic mode (at the cost of execution speed).')
+parser.add_argument('-d', '--data', type=str, default='./data.h5', help='training data')
+parser.add_argument('-b', '--batch_size', type=int, default=4, help='training batch size')
+parser.add_argument('--delta', default=(5, 5, 5), help='delta offset')
+parser.add_argument('--input_size', default=(31, 31, 31), help='input size')
+parser.add_argument('--clip_grad_thr', type=float, default=0.7, help='grad clip threshold')
+parser.add_argument('--save_path', type=str, default='./model', help='model save path')
+parser.add_argument('--resume', type=str, default=None, help='resume training')
+parser.add_argument('--interval', type=int, default=120, help='How often to save model (in seconds).')
+parser.add_argument('--iter', type=int, default=1e100, help='training iteration')
+
+parser.add_argument('--inspection_path', type=str, default='./inspection', help='inspect training process')
+args = parser.parse_args()
+
+deterministic = args.deterministic
+if deterministic:
+    torch.backends.cudnn.deterministic = True
+else:
+    torch.backends.cudnn.benchmark = True  # Improves overall performance in *most* cases
+
+if not os.path.exists(args.save_path):
+    os.makedirs(args.save_path)
+if not os.path.exists(args.inspection_path):
+    os.makedirs(args.inspection_path)
+
+def run():
+    """创建模型"""
+    model = FFN(in_channels=2, out_channels=1, input_size=args.input_size, delta=args.delta).cuda()
+
+    if args.resume is not None:
+        model.load_state_dict(torch.load(args.resume))
+
+    """数据路径"""
+    input_h5data = [args.data]
+
+    """创建data loader"""
+    train_dataset = BatchCreator(input_h5data, args.input_size, delta=args.delta, train=True)
+    train_loader = DataLoader(train_dataset, shuffle=True, num_workers=1, pin_memory=True)
+
+    optimizer = optim.SGD(model.parameters(), lr=1e-3)
+
+    best_loss = np.inf
+
+    """获取数据流"""
+    t_last = time.time()
+    cnt = 0
+    iter_i = 0
+    tp = fp = tn = fn = 0
+    while cnt < args.iter:
+        cnt += 1
+        for iter, (seeds, images, labels, offsets) in enumerate(
+                get_batch(train_loader, args.batch_size, args.input_size,
+                          partial(fixed_offsets, fov_moves=train_dataset.shifts))):
+
+            t_curr = time.time()
+            """正样本权重"""
+            pos_w = - torch.log((labels > 0.5).sum().float() / np.prod(labels.shape))
+            slice = seeds[:, :, seeds.shape[2] // 2, :, :].sigmoid()
+            seeds[:, :, seeds.shape[2] // 2, :, :] = slice
+            labels = labels.cuda()
+
+            input_data = torch.cat([images, seeds], dim=1)
+            input_data = Variable(input_data.cuda())
+
+            logits = model(input_data)
+            updated = seeds.cuda() + logits
+
+            optimizer.zero_grad()
+            loss = F.binary_cross_entropy_with_logits(updated, labels, pos_weight=pos_w)
+            loss.backward()
+            """梯度截断"""
+            torch.nn.utils.clip_grad_value_(model.parameters(), args.clip_grad_thr)
+            optimizer.step()
+
+            # update_seed(updated, seeds, model, offsets)
+            seeds = updated
+            iter_i += 1
+            if iter_i % 1000 == 999:
+                input_data_i = input_data.cpu().detach().numpy()
+                updated_i = updated.cpu().detach().numpy()
+                inspection_save_path = 'inspection_' + str(cnt) + '_' + str(iter) + '.h5'
+                with h5py.File(os.path.join(args.inspection_path, inspection_save_path), 'w') as f:
+                    f.create_dataset('input_data', data=input_data_i, compression='gzip')
+                    f.create_dataset('updated', data=updated_i, compression='gzip')
+
+            pred_mask = (updated >= logit(0.9)).detach().cpu().numpy()
+            true_mask = (labels > 0.5).cpu().numpy()
+            true_bg = np.logical_not(true_mask)
+            pred_bg = np.logical_not(pred_mask)
+            tp += (true_mask & pred_mask).sum()
+            fp += (true_bg & pred_mask).sum()
+            fn += (true_mask & pred_bg).sum()
+            tn += (true_bg & pred_bg).sum()
+            precision = 1.0 * tp / max(tp + fp, 1)
+            recall = 1.0 * tp / max(tp + fn, 1)
+            accuracy = 1.0 * (tp + tn) / (tp + tn + fp + fn)
+            count = round(iter / len(train_loader) * 50)
+            sys.stdout.write('[Epoch {}], {}/{}: [{}{}] loss: {:.4}, Precision: {:.2f}%, Recall: {:.2f}%, '
+                             'Accuracy: {:.2f}%\r'.format(cnt, (iter + 1) * args.batch_size, len(train_loader),
+                                                          '#' * count, ' ' * (50 - count), loss.item(), precision*100,
+                                                          recall*100, accuracy * 100))
+
+            """根据最佳loss并且保存模型"""
+            if best_loss > loss.item() or t_curr - t_last > args.interval:
+                tp = fp = tn = fn = 0
+                t_last = t_curr
+                best_loss = loss.item()
+                torch.save(model.state_dict(), os.path.join(args.save_path, 'ffn.pth'))
+                print('Precision: {:.2f}%, Recall: {:.2f}%, Accuracy: {:.2f}%, Model saved!'.format(
+                    precision * 100, recall * 100, accuracy * 100))
+
+
+if __name__ == "__main__":
+    seed = int(time.time())
+    random.seed(seed)
+
+    run()
