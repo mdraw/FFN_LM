442e08958bacb3342c50949b6e402c59a58dd7fd
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2019-11-06 17:27:42 -0500

    Update inference_run.py

--- inference_run.py
+++ inference_run.py
@@ -1,20 +1,16 @@
 import os
-import sys
-import numpy as np
-import time
-from scipy.special import expit
-from scipy.special import logit
 import h5py
 import argparse
 from core.models.ffn import FFN
 from core.data.utils import *
 
 parser = argparse.ArgumentParser(description='inference script')
-parser.add_argument('--data', type=str, default='./images.h5', help='input images')
-parser.add_argument('--label', type=str, default='./groundtruth.h5', help='input images')
-parser.add_argument('--model', type=str, default='./model/ffn1.pth', help='path to ffn model')
-parser.add_argument('--delta', default=(5, 5, 5), help='delta offset')
-parser.add_argument('--input_size', default=(33, 33, 33), help='input size')
+parser.add_argument('--data', type=str, default='./data.h5', help='input images')
+parser.add_argument('--label', type=str, default='./pred.h5', help='input images')
+parser.add_argument('--model', type=str, default='./model/raw_3_with_color_15_15_15_3_3_3_final.pth', help='path to ffn model')
+parser.add_argument('--delta', default=(3, 3, 3), help='delta offset')
+parser.add_argument('--input_size', default=(31, 31, 31), help='input size')
+parser.add_argument('--depth', type=int, default=12, help='depth of ffn')
 parser.add_argument('--seg_thr', type=float, default=0.6, help='input size')
 parser.add_argument('--mov_thr', type=float, default=0.9, help='input size')
 parser.add_argument('--act_thr', type=float, default=0.95, help='input size')
@@ -23,21 +19,39 @@ args = parser.parse_args()
 
 
 def run():
-    model = FFN(in_channels=2, out_channels=1, input_size=args.input_size, delta=args.delta).cuda()
+    """创建模型"""
+    model = FFN(in_channels=4, out_channels=1, input_size=args.input_size, delta=args.delta, depth=args.depth).cuda()
 
     assert os.path.isfile(args.model)
 
+    """载入模型"""
     model.load_state_dict(torch.load(args.model))
 
+    """读取数据"""
     with h5py.File(args.data, 'r') as f:
-        images = (f['raw'].value.astype(np.float32) - 128) / 33
+        images = (f['image'][()].astype(np.float32) - 128) / 33
         # labels = g['label'].value
 
+    """创建分割实例"""
     canva = Canvas(model, images, args.input_size, args.delta, args.seg_thr, args.mov_thr, args.act_thr)
+    """开始分割"""
     canva.segment_all()
+    """获取结果"""
+    # result = canva.segmentation
+    # """存储结果"""
+    # max_value = result.max()
+    # indice, count = np.unique(result, return_counts=True)
+    # result[result == -1] = 0
+    # result = result*(1.0 * 255/max_value)
+    rlt_key = []
+    rlt_val = []
+    result = canva.target_dic
     with h5py.File(args.label, 'w') as g:
-        f.create_dataset('raw', data=canva.segmentation, compression='gzip')
-    print('done')
+        for key, value in result.items():
+            rlt_key.append(key)
+            rlt_val.append((value > 0).sum())
+            g.create_dataset('id_{}'.format(key), data=value.astype(np.uint8), compression='gzip')
+    print('label: {}, number: {}'.format(rlt_key, rlt_val))
 
 
 if __name__ == '__main__':
